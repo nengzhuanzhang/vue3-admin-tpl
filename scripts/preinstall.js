10625298adbf2729ba41c3b05bb23a965635ec83
Bella Zhang <bella_zhang@epam.com>
2024-03-28 17:07:15 +0800

    style: file format

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,10 +1,9 @@
-
 // 如果要改成yarn只需要把pnpm改为yarn就行
 if (!/pnpm/.test(process.env.npm_execpath || '')) {
   // 提示语，告诉别人只能使用pnpm下载
   console.warn(
     `\u001b[33mThis repository must using pnpm as the package manager ` +
-    ` for scripts to work properly.\u001b[39m\n`,
+      ` for scripts to work properly.\u001b[39m\n`,
   )
   process.exit(1)
 }
