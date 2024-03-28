3f59b122c8ccc10acd9e3dab9dbfe439d6cfdd5c
Bella Zhang <bella_zhang@epam.com>
2024-03-28 16:37:48 +0800

    test husky

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+const testFun = () => {
+  console.log(1)
+}
+testFun()
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <div>测试格式化</div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
 .logo {
   height: 6em;
+
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
